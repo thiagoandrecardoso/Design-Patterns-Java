d0cc4699f9275d73d87dd852c1925d063314506c
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2020-06-20 19:43:13 -0300

    Added strategy product

---
+++ src/strategy/userd/Product.java
@@ -0,0 +1,28 @@
+package strategy.userd;
+
+public class Product extends ProductAbs{
+    private float price;
+    private int amount;
+
+    public Product(float price, int amount, ProductType productType) {
+        super(productType);
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/strategy/userd/ProductType.java
@@ -0,0 +1,6 @@
+package strategy.userd;
+
+public interface ProductType {
+    float calcFinalValue();
+    void setProduct(Product product);
+}
