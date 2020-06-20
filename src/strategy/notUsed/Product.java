7cc421ac4679130bcaab3b2e33a881227d8f9074
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2020-06-20 12:21:58 -0300

    Added another observer exemple
    Added misuse of strategy

---
+++ src/strategy/notUsed/Product.java
@@ -0,0 +1,75 @@
+package strategy.notUsed;
+
+public class Product {
+
+    private float price;
+    private int amount;
+    private int type;
+
+    public Product(float price, int amount, int type) {
+        this.price = price;
+        this.amount = amount;
+        this.type = type;
+    }
+
+    public float calcFinalValue() {
+        float finalValue = (float) 0.0;
+
+        if (type == 1) {
+            if (amount <= 10) {
+                finalValue = price * amount;
+            } else if (amount <= 50) {
+                finalValue = (float) ((price * amount) * 0.9);
+            } else if (amount <= 100) {
+                finalValue = (float) ((price * amount) * 0.8);
+            } else {
+                finalValue = (float) ((price * amount) * 0.7);
+            }
+        } else if (type == 2) {
+            if (amount <= 10) {
+                finalValue = price * amount;
+            } else if (amount <= 50) {
+                finalValue = (float) ((price * amount) * 0.8);
+            } else if (amount <= 100) {
+                finalValue = (float) ((price * amount) * 0.7);
+            } else {
+                finalValue = (float) ((price * amount) * 0.6);
+            }
+        } else if (type == 3) {
+            if (amount <= 10) {
+                finalValue = price * amount;
+            } else if (amount <= 50) {
+                finalValue = (float) ((price * amount) * 0.7);
+            } else if (amount <= 100) {
+                finalValue = (float) ((price * amount) * 0.6);
+            } else {
+                finalValue = (float) ((price * amount) * 0.5);
+            }
+        }
+        return finalValue;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
---
+++ src/strategy/notUsed/RunProduct.java
@@ -0,0 +1,10 @@
+package strategy.notUsed;
+
+public class RunProduct {
+
+    public static void main(String[] args) {
+
+        Product product = new Product((float) 100.00, 21, 2);
+        System.out.println(product.calcFinalValue());
+    }
+}
